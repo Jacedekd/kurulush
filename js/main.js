28ae3a9c8a4c5ca993bd09b71d39d75846373c1e
Aziz <azizkazanbaev@gmail.com>
2022-12-20 16:59:12 +0600

    commit

--- js/main.js
+++ js/main.js
@@ -31,12 +31,7 @@ let select2 = document.querySelector('#select');
 
 let full_Price_P = document.querySelector('.full_Price_P')
 
-select.addEventListener('click', () => {
-    let value = Number(select.value)
 
-    let nemValue = value / 120
-    full_Price_P.textContent = nemValue + '$'
-})
 
 let cbox1 = document.querySelector('#cbox1')
 let cbox2 = document.querySelector('#cbox2')
@@ -46,28 +41,70 @@ let cbox4 = document.querySelector('#cbox4')
 let arr = []
 arr.push(cbox1, cbox2, cbox3, cbox4)
 arr.forEach(e => {
+    
     e.addEventListener('click', () => {
-      
+        
+        
         let value = Number(select.value)
-
+        if (e.checked === true) {
+            e.classList.add('active')
+        }
         if (e.id === 'cbox1' && e.checked) {
-                full_Price_P.textContent = (value - (value * (10 / 100))) / 120 + '$'
+
+            full_Price_P.textContent = Math.round((value - (value * (10 / 100))) / 120) + '.00$'
         }
         if (e.id === 'cbox2' && e.checked) {
-            full_Price_P.textContent = (value - (value * (20 / 100))) / 120 + '$'
+
+            full_Price_P.textContent = Math.round((value - (value * (20 / 100))) / 120) + '.00$'
         }
         if (e.id === 'cbox3' && e.checked) {
-            full_Price_P.textContent = (value - (value * (30 / 100))) / 120 + '$'
+            full_Price_P.textContent = Math.round((value - (value * (30 / 100))) / 120) + '.00$'
         }
         if (e.id === 'cbox4' && e.checked) {
-            full_Price_P.textContent = value / 120 + '$'
-        }
-        if (e.id === 'cbox4' && e.checked) {
-            full_Price_P.textContent = value / 120 + '$'
+            full_Price_P.textContent = Math.round(value / 120) + '.00$'
         }
 
     })
 })
 
 
+select.addEventListener('click', () => {
+    let value = Number(select.value)
 
+
+    let nemValue = value / 120
+    // full_Price_P.textContent = nemValue + '$'
+
+    arr.forEach(e => {
+        if (e.checked === true) {
+
+            if (e.id === 'cbox4') {
+                full_Price_P.textContent = Math.round(value / 120) + '.00$'
+            }
+            if (e.id === 'cbox1') {
+                full_Price_P.textContent = Math.round((value - (value * (10 / 100))) / 120) + '.00$'
+            }
+            if (e.id === 'cbox2') {
+                full_Price_P.textContent = Math.round((value - (value * (20 / 100))) / 120) + '.00$'
+            }
+            if (e.id === 'cbox3') {
+                full_Price_P.textContent = Math.round((value - (value * (30 / 100))) / 120) + '.00$'
+            }
+        }
+        // if (e.id === 'cbox1' && e.checked) {
+        //     full_Price_P.textContent = (value - (value * (10 / 100))) / 120 + '$'
+        // }
+        // if (e.id === 'cbox2' && e.checked) {
+        //     full_Price_P.textContent = (value - (value * (20 / 100))) / 120 + '$'
+        // }
+        // if (e.id === 'cbox3' && e.checked) {
+        //     full_Price_P.textContent = (value - (value * (30 / 100))) / 120 + '$'
+        // }
+        // if (e.id === 'cbox4' && e.checked) {
+        //     full_Price_P.textContent = value / 120 + '$'
+        // }
+        // if (e.id === 'cbox4' && e.checked) {
+        //     full_Price_P.textContent = value / 120 + '$'
+        // }
+    })
+})
